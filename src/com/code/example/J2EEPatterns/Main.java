11acdeaa83cfaf1d991e9c33cc8e9b1210de78ab
童成钰 <1429920627@qq.com>
2020-12-18 11:02:35 +0800

    文件夹占位

---
+++ src/com/code/example/J2EEPatterns/Main.java
@@ -0,0 +1,8 @@
+package com.code.example.J2EEPatterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/com/code/example/structuralPatterns/Main.java
@@ -0,0 +1,8 @@
+package com.code.example.structuralPatterns;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
